3948a9f209b365e025abb58d3e1ed3a713a5fb72
DRBriseno <rose.briseno@students.makeschool.com>
2021-03-23 14:47:21 -0700

    barely done, has bugs

---
+++ app.js
@@ -0,0 +1,79 @@
+const app = () => {
+  const song= document.querySelector('.song');
+  const song= document.querySelector('.play');
+  const outline= document.querySelector('.moving-outline circle');
+  const video= document.querySelector('.vid-container video');
+
+
+  /////sounds
+
+
+  const sounds= document.querySelectorAll('.sound-picker button');
+
+
+  ///////time-display
+
+
+  const timeDisplay= document.querySelector('.time-display');
+  
+  
+/////// get the length of the outline
+
+    const outlineLength= outline.getTotalLength();
+    console.log(outlineLength);
+
+
+   ///////duration
+   
+   
+    let fakeDuration= 600;
+    
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+
+///////play sound
+
+    play.addEventListener('click', () => {
+       checkPlaying(song);
+    });
+
+
+    /////create a funtion specific to stop and play the song
+
+    const checkPlaying = song => {
+
+    
+    if(song.paused){
+        song.play();
+        video.play();
+        play.scr = './svg/pause.svg';
+    }else{
+        song.pause();
+        video.pause();
+        play.src = './svg/play.svg';
+    }
+}
+
+//////animate the circle
+
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+
+
+        ////////animate the circle
+
+        let progress = outlineLength - ( currentTime / fakeDuration)   * outlineLength;
+        outline.style.strokeDashoffset = progress;
+    }
+
+
+
+};
+
+
+app();
